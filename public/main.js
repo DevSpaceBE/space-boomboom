37faf3a1190ee2d0019029c235cd1f67b9ffcbbf
Romain Gérard <romaingweb@gmail.com>
2016-11-26 00:31:08 +0100

    Use P2 physics

--- public/main.js
+++ public/main.js
@@ -1,32 +1,23 @@
-const game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { create: create, render: render })
-
-const UP_DISTANCE = 1
-
-
-
-
+const game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { create: create, render: render, preload: preload })
 
 let velocity = 0, keyUp, ship
 
+function preload() {
+    game.load.image('ship', 'ship.jpg')
+}
+
 function create() {
+    game.physics.startSystem(Phaser.Physics.P2JS)
     keyUp = game.input.keyboard.addKey(Phaser.Keyboard.UP)
     keyUp.onHoldCallback = accelerate
     keyDown = game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
     keyDown.onHoldCallback = decelerate
 
-    const shipPoly = new Phaser.Polygon();
-    //  And then populate it via setTo, using any combination of values as above
-    shipPoly.setTo([new Phaser.Point(20, 0), new Phaser.Point(0, 50), new Phaser.Point(40, 50)]);
-    ship = game.add.graphics(375, 550);
-
-    ship.beginFill(0xFF33ff)
-    ship.drawPolygon(shipPoly.points)
-    ship.endFill()
-
-
-    setInterval(function () {
-      ship.y -= velocity
-    }, 10)
+    ship = game.add.sprite(375, 550, 'ship')
+    ship.width = 60
+    ship.height = 70
+    game.physics.p2.enable(ship)
+    console.log(ship.body)
 }
 
 function render() {
@@ -34,7 +25,7 @@ function render() {
 }
 
 function accelerate() {
-    velocity += 1
+    ship.body.moveUp(40)
 }
 
 function decelerate() {
